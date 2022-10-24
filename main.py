9697ea9fdc2f5297416f8c164b9ae71ea536359f
Pioneer-er <Pioneer-er@mail.ru>
2022-10-24 23:21:29 +0700

    update code

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ def attack(char_name, char_class):
     if char_class == 'healer':
         return ('''f'{char_name} нанёс урон противнику равный
                                 {5 + randint(-3, -1)}''')
-    else:
-        return (f'{char_name} промахнулся!')
+    return (f'{char_name} промахнулся!')
 
 
 def defence(char_name, char_class):
@@ -22,8 +21,8 @@ def defence(char_name, char_class):
         return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
     if char_class == 'healer':
         return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
-    else:
-        return (f' {char_name} получил урон')
+
+    return (f' {char_name} получил урон')
 
 
 def special(char_name, char_class):
@@ -36,8 +35,7 @@ def special(char_name, char_class):
     if char_class == 'healer':
         return ('''f'{char_name} применил специальное умение
                                 «Защита {10 + 30}»''')
-    else:
-        return (f'{char_name} не удалось применить умение')
+    return (f'{char_name} не удалось применить умение')
 
 
 def start_training(char_name, char_class):
